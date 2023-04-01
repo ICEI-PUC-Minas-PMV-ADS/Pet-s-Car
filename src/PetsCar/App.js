f10bbb7f81ec60665338e6e11bf5868ce74ec5bf
joaojorges <joaojorges002@gmail.com>
2023-04-01 12:30:45 -0300

    add pagina login cliente

--- src/PetsCar/App.js
+++ src/PetsCar/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomePage from './pages/Mariano/welcomePage';
+import LoginCliente from './pages/JoaoJorges/loginCliente';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <WelcomePage/>
+      <LoginCliente/>
       
     </View>
   );
---
+++ src/PetsCar/pages/JoaoJorges/loginCliente.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+
+const LoginCliente = ()=>{
+
+    return(
+        <Text>Login Cliente</Text>
+    )
+}
+
+export default LoginCliente
