4ee2feb385ae90dc46e896d70482ea6e3eca46ba
joaojorges <joaojorges002@gmail.com>
2023-04-02 14:29:39 -0300

    att pagijna login cliente

--- src/PetsCar/pages/JoaoJorges/loginCliente.js
+++ src/PetsCar/pages/JoaoJorges/loginCliente.js
@@ -1,10 +1,26 @@
-import { Text } from 'react-native';
+// João Jorges - Desenvolvi o ínicio da tela utilizando o material disponível na disciplina e com a ajuda do Thiago
+import { StyleSheet, Text } from 'react-native';
 
-const LoginCliente = ()=>{
-
-    return(
-        <Text>Login Cliente</Text>
-    )
+const LoginCliente = () => { 
+    return (
+        <>
+        <Text styles={styles.title}>Login de Cliente</Text>
+        <Text styles={styles.subtitle}>Insira seu e-mail e senha e faça seu agendamento!</Text>
+        </>
+    ) 
 }
 
-export default LoginCliente
+const styles = StyleSheet.create({
+    title: {
+      color: '#4060FF',
+      fontSize: '32px',
+      fontWeight: '700', 
+    },
+
+    subtitle: {
+      color: '#131313',
+      fontSize: '16px',
+      fontWeight: '400', 
+
+    }
+  });
